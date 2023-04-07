aa2659d7bf4754047b6d3c0a8c065975d571acb3
Emil Stanchev <emilstan4ev03@gmail.com>
2023-04-07 23:28:40 +0300

    Added client side to the project

---
+++ SongApplication/Program.cs
@@ -0,0 +1,54 @@
+using Microsoft.EntityFrameworkCore;
+using Services.Contracts;
+using Services.HelpingClasses;
+using Services.PrintInterfaces;
+using Services.Services;
+using Services.ViewModels;
+using SongApplication.Infrastructure.Data;
+using SongApplication.Models.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<ISongCrudOperations, SongCrudOperations>();
+builder.Services.AddScoped<IPrintMessage, Print>();
+builder.Services.AddAutoMapper(o =>
+{
+    o.CreateMap<Song, SongViewModel>().ReverseMap();
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
